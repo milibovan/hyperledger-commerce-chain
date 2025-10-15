649463d29b19c6f75569595d9911824acaefdf24
milibovan <milibovan190d@gmail.com>
2025-10-15 15:25:41 +0200

    Created functions for creating product and user

--- go/src/github.com/milibovan/application/client/contract.go
+++ go/src/github.com/milibovan/application/client/contract.go
@@ -9,7 +9,7 @@ import (
 
 func CreateTrader(gw *fabricClient.Gateway, channel, traderType, vat, balance string) (uint64, string) {
 	var now = time.Now()
-	var ID = fmt.Sprintf("%d", now.UnixNano())
+	var ID = fmt.Sprintf("TRADER_%d", now.UnixNano())
 
 	net := gw.GetNetwork(channel)
 	ccContract := net.GetContract(ChaincodeName)
@@ -31,6 +31,62 @@ func CreateTrader(gw *fabricClient.Gateway, channel, traderType, vat, balance st
 	}
 
 	fmt.Println("\n*** CreateTrader committed successfully")
+
+	return status.BlockNumber, ID
+}
+
+func CreateUser(gw *fabricClient.Gateway, channel, name, surname, email, balance string) (uint64, string) {
+	var now = time.Now()
+	var ID = fmt.Sprintf("USER_%d", now.UnixNano())
+
+	net := gw.GetNetwork(channel)
+	ccContract := net.GetContract(ChaincodeName)
+
+	fmt.Printf("\n--> Submit transaction: CreateUser, ID: %s on channel %s\n", ID, channel)
+
+	_, commit, err := ccContract.SubmitAsync("CreateUser", fabricClient.WithArguments(ID, name, surname, email, balance))
+	if err != nil {
+		panic(fmt.Errorf("failed to submit transaction: %w", err))
+	}
+
+	status, err := commit.Status()
+	if err != nil {
+		panic(fmt.Errorf("failed to get transaction commit status: %w", err))
+	}
+
+	if !status.Successful {
+		panic(fmt.Errorf("failed to commit transaction with status code %v", status.Code))
+	}
+
+	fmt.Println("\n*** CreateUser committed successfully")
+
+	return status.BlockNumber, ID
+}
+
+func CreateProduct(gw *fabricClient.Gateway, channel, name, expiryDate, price, quantity, traderTypeStr string) (uint64, string) {
+	var now = time.Now()
+	var ID = fmt.Sprintf("PRODUCT_%d", now.UnixNano())
+
+	net := gw.GetNetwork(channel)
+	ccContract := net.GetContract(ChaincodeName)
+
+	fmt.Printf("\n--> Submit transaction: CreateProduct, ID: %s on channel %s\n", ID, channel)
+
+	_, commit, err := ccContract.SubmitAsync("CreateProduct", fabricClient.WithArguments(ID, name, expiryDate, price, quantity, traderTypeStr))
+	if err != nil {
+		panic(fmt.Errorf("failed to submit transaction: %w", err))
+	}
+
+	status, err := commit.Status()
+	if err != nil {
+		panic(fmt.Errorf("failed to get transaction commit status: %w", err))
+	}
+
+	if !status.Successful {
+		panic(fmt.Errorf("failed to commit transaction with status code %v", status.Code))
+	}
+
+	fmt.Println("\n*** CreateProduct committed successfully")
 
 	return status.BlockNumber, ID
 }
--- go/src/github.com/milibovan/chaincode/creates.go
+++ go/src/github.com/milibovan/chaincode/creates.go
@@ -11,13 +11,18 @@ import (
 	"github.com/hyperledger/fabric-contract-api-go/v2/contractapi"
 )
 
-func (s *SmartContract) CreateUser(ctx contractapi.TransactionContextInterface, id string, name string, surname string, email string, balance float64) error {
+func (s *SmartContract) CreateUser(ctx contractapi.TransactionContextInterface, id, name, surname, email, balance string) error {
 	exists, err := s.AssetExists(ctx, id, "user")
 	if err != nil {
 		return err
 	}
 	if exists {
 		return fmt.Errorf("User %s already exists", id)
+	}
+
+	balanceFl, err := strconv.ParseFloat(balance, 64)
+	if err != nil {
+		return fmt.Errorf("invalid balance format: %w", err)
 	}
 
 	user := structs.User{
@@ -27,7 +32,7 @@ func (s *SmartContract) CreateUser(ctx contractapi.TransactionContextInterface, 
 		Surname:     surname,
 		Email:       email,
 		ReceiptsIDs: []string{},
-		Balance:     balance,
+		Balance:     balanceFl,
 	}
 
 	userJSON, err := json.Marshal(user)
@@ -35,7 +40,7 @@ func (s *SmartContract) CreateUser(ctx contractapi.TransactionContextInterface, 
 		return err
 	}
 
-	return ctx.GetStub().PutState("USER_"+user.Id, userJSON)
+	return ctx.GetStub().PutState(user.Id, userJSON)
 }
 
 func (s *SmartContract) CreateTrader(ctx contractapi.TransactionContextInterface, id, traderTypeStr, vat, balance string) error {
@@ -72,17 +77,32 @@ func (s *SmartContract) CreateTrader(ctx contractapi.TransactionContextInterface
 		return err
 	}
 
-	return ctx.GetStub().PutState("TRADER_"+trader.Id, traderJSON)
+	return ctx.GetStub().PutState(trader.Id, traderJSON)
 }
 
-func (s *SmartContract) CreateProduct(ctx contractapi.TransactionContextInterface, id string, name string, expiryDate time.Time, price float64, quantity int, traderTypeStr string) error {
+func (s *SmartContract) CreateProduct(ctx contractapi.TransactionContextInterface, id, name, expiryDate, price, quantity, traderTypeStr string) error {
 	exists, err := s.AssetExists(ctx, id, "product")
 	if err != nil {
 		return err
 	}
+
+	layout := "2006-01-02 15:04:05"
+	expiryDateTime, err := time.Parse(layout, expiryDate)
+	if err != nil {
+		return fmt.Errorf("invalid expiry date format: %w", err)
+	}
+
+	priceFl, err := strconv.ParseFloat(price, 64)
+	if err != nil {
+		return fmt.Errorf("invalid balance format: %w", err)
+	}
+
+	quantityInt, err := strconv.Atoi(quantity)
+
 	if exists {
 		return fmt.Errorf("Product %s already exists", id)
 	}
+
 	traderType, err := structs.GetTraderTypeFromString(traderTypeStr)
 	if err != nil {
 		return err
@@ -92,9 +112,9 @@ func (s *SmartContract) CreateProduct(ctx contractapi.TransactionContextInterfac
 		DocType:    "product",
 		Id:         id,
 		Name:       name,
-		Price:      price,
-		ExpiryDate: expiryDate,
-		Quantity:   quantity,
+		Price:      priceFl,
+		ExpiryDate: expiryDateTime,
+		Quantity:   quantityInt,
 		TraderType: traderType,
 	}
 
@@ -103,7 +123,7 @@ func (s *SmartContract) CreateProduct(ctx contractapi.TransactionContextInterfac
 		return err
 	}
 
-	return ctx.GetStub().PutState("PRODUCT_"+product.Id, productJSON)
+	return ctx.GetStub().PutState(product.Id, productJSON)
 }
 
 func (s *SmartContract) CreateReceipt(ctx contractapi.TransactionContextInterface, traderId string, userId string, productIds []string) (string, error) {
@@ -123,5 +143,5 @@ func (s *SmartContract) CreateReceipt(ctx contractapi.TransactionContextInterfac
 		return "", err
 	}
 
-	return id, ctx.GetStub().PutState("RECEIPT_"+receipt.Id, receiptJSON)
+	return id, ctx.GetStub().PutState(receipt.Id, receiptJSON)
 }
