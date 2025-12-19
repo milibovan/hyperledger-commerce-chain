0abe35ffe77911af27f8f973155ca48646f75c94
Mili Bovan <milibovan190d@gmail.com>
2025-12-19 17:50:11 +0100

    feat: Rebased architecture branch

--- commerce-chain-frontend/src/main.tsx
+++ commerce-chain-frontend/src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
-import DetailedArchitectureDiagram from './components/reusables/DetailedArchitectureDiagram.tsx'
+import App from './App.tsx'
+// import DetailedArchitectureDiagram from './components/reusables/DetailedArchitectureDiagram.tsx'
 // import ArchitectureDiagram from './components/reusables/ArchitectureDiagram.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <DetailedArchitectureDiagram />
+    <App />
+    {/* <DetailedArchitectureDiagram /> */}
     {/* <ArchitectureDiagram /> */}
   </StrictMode>,
 )
