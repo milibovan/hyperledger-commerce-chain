6af9504b00a91c9a411179d4aaa68206c1b67566
Mili Bovan <milibovan190d@gmail.com>
2025-12-19 17:46:11 +0100

    feat: Added detailed architecture diagram

--- commerce-chain-frontend/src/main.tsx
+++ commerce-chain-frontend/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+import DetailedArchitectureDiagram from './components/reusables/DetailedArchitectureDiagram.tsx'
 // import ArchitectureDiagram from './components/reusables/ArchitectureDiagram.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <DetailedArchitectureDiagram />
     {/* <ArchitectureDiagram /> */}
   </StrictMode>,
 )
