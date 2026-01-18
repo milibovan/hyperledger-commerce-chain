9276c5a3a9de7c214bb808e3949244b724435325
milibovan <milibovan190d@gmail.com>
2026-01-18 01:00:44 +0100

    feat: Defined basic auth server and added postgres to docker compose. TODO Define routes, models and sql scripts and implement logic

--- auth-service/auth-service/src/main.rs
+++ auth-service/auth-service/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, World!"
+}
+
+
+// TODO Routes
+// /login
+// /register
+// /verify
+// Define models and sql scripts
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+        .bind(("127.0.0.1", env::var("PORT").unwrap_or("5050".to_string())))?
+        .run()
+        .await
 }
