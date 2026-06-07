939e95f9091a972e472f033e95d8be2826cd957b
Mili Bovan <milibovan190d@gmail.com>
2026-06-08 00:18:17 +0200

    fix: Fixed confluent resources. TODO Test all together

--- terraform/modules/confluent-base/main.tf
+++ terraform/modules/confluent-base/main.tf
@@ -34,12 +34,38 @@ resource "confluent_api_key" "schema_registry_key" {
   }
 
   managed_resource {
-    id          = data.confluent_schema_registry_cluster.essentials.id
-    api_version = data.confluent_schema_registry_cluster.essentials.api_version
-    kind        = data.confluent_schema_registry_cluster.essentials.kind
+    id          = data.confluent_schema_registry_cluster.schema_registry.id
+    api_version = data.confluent_schema_registry_cluster.schema_registry.api_version
+    kind        = data.confluent_schema_registry_cluster.schema_registry.kind
 
     environment {
       id = var.env_id
     }
   }
+}
+
+resource "confluent_api_key" "kafka_cluster_key" {
+  display_name = "kafka-cluster-api-key"
+  
+  owner {
+    id          = confluent_service_account.app_sa.id
+    api_version = confluent_service_account.app_sa.api_version
+    kind        = confluent_service_account.app_sa.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.kafka_cluster.id
+    api_version = confluent_kafka_cluster.kafka_cluster.api_version
+    kind        = confluent_kafka_cluster.kafka_cluster.kind
+
+    environment {
+      id = var.env_id
+    }
+  }
+}
+
+resource "confluent_role_binding" "app_sa_kafka_admin" {
+  principal   = "User:${confluent_service_account.app_sa.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.kafka_cluster.rbac_crn
 }
