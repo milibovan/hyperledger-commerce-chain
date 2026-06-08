55f681b9bf08f22452110cb67b71efb724c2319c
Mili Bovan <milibovan190d@gmail.com>
2026-06-09 00:23:52 +0200

    fix: fixed fields after change for all envs

--- terraform/envs/az-free/main.tf
+++ terraform/envs/az-free/main.tf
@@ -8,6 +8,17 @@ resource "random_string" "suffix" {
   special = false
 }
 
+module "networking" {
+  source                        = "../../modules/networking"
+  vnet_address_space            = var.vnet_address_space
+  compute_subnet_address_prefix = var.compute_subnet_address_prefix
+  name_suffix                   = local.name_suffix
+  project_name                  = local.project_name
+  tags                          = local.tags
+  name                          = azurerm_resource_group.main.name
+  location                      = azurerm_resource_group.main.location
+}
+
 module "frontend" {
   source       = "../../modules/static-web-app"
   name         = azurerm_resource_group.main.name
@@ -26,13 +37,23 @@ resource "azurerm_resource_group" "main" {
   }, local.tags)
 }
 
+module "container_app_env_main" {
+  source       = "../../modules/container-app-environment"
+  name         = azurerm_resource_group.main.name
+  location     = azurerm_resource_group.main.location
+  name_suffix  = local.name_suffix
+  project_name = local.project_name
+  subnet_id    = module.networking.azurerm_subnet_id
+}
+
 module "container_apps" {
-  source              = "../../modules/container-app"
-  name_suffix         = local.name_suffix
-  project_name        = local.project_name
-  name                = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  cpu_allocation      = 0.25
-  memory_allocation   = "0.5Gi"
-  apps                = ["go-backend", "email-service", "stream-generator"]
+  source                       = "../../modules/container-app"
+  name_suffix                  = local.name_suffix
+  project_name                 = local.project_name
+  name                         = azurerm_resource_group.main.name
+  location                     = azurerm_resource_group.main.location
+  container_app_environment_id = module.container_app_env_main.env_id
+  cpu_allocation               = 0.25
+  memory_allocation            = "0.5Gi"
+  apps                         = ["go-backend", "email-service", "stream-generator"]
 }
